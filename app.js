4a8efe51ac4c73a3f58463a7f8c23441c4d3af43
jahallb1 <62314714+jahallb1@users.noreply.github.com>
2020-07-29 16:09:02 -0400

    added inquirer to app.js

--- app.js
+++ app.js
@@ -1,14 +1,31 @@
-const fs = require('fs');
+const inquirer = require('inquirer');
 
-const generatePage = require('./src/page-template.js');
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'
+        }
+    ])
+    .then(answers => console.log(answers));
 
-const profileDateArgs = process.argv.slice(2);
-
-const [name, github] = profileDateArgs;
 
 
-fs.writeFile('./index.html', generatePage(name, github), err => {
-    if (err) throw new Error(err);
 
-    console.log('Portfolio complete! Check out index.html to see the output!');
-});
+
+
+// const fs = require('fs');
+
+// const generatePage = require('./src/page-template.js');
+
+// const profileDateArgs = process.argv.slice(2);
+
+// const [name, github] = profileDateArgs;
+
+
+// fs.writeFile('./index.html', generatePage(name, github), err => {
+//     if (err) throw new Error(err);
+
+//     console.log('Portfolio complete! Check out index.html to see the output!');
+// });
