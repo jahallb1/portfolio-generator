0b70022a06121b6995660c2b5ef22ac2dd7bbe32
jahallb1 <62314714+jahallb1@users.noreply.github.com>
2020-07-30 15:18:49 -0400

    added the validate features

--- app.js
+++ app.js
@@ -5,12 +5,27 @@ const promptUser = () => {
         {
             type: 'input',
             name: 'name',
-            message: 'What is your name?'
+            message: 'What is your name? (Required)',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your name!');
+                    return false;
+                }
+            }
         },
         {
             type: 'input',
             name: 'github',
-            message: 'Enter your GitHub Username'
+            message: 'Enter your GitHub Username (Required)',
+            validate: githubInput => {
+                if (githubInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your github id!');
+                }
+            }
         },
         {
             type: 'input',
@@ -35,12 +50,28 @@ const promptProject = portfolioData => {
           {
               type: 'input',
               name: 'name',
-              message: 'What is the name of your project?'
+              message: 'What is the name of your project? (Required)',
+              validate: projectNameInput => {
+                  if (projectNameInput) {
+                      return true;
+                  } else {
+                      console.log('Please enter your projcet name!');
+                      return false;
+                  }
+              } 
           },
           {
             type: 'input',
             name: 'description',
-            message: 'Provide a description of the project (Required)'
+            message: 'Provide a description of the project (Required)',
+            validate: descriptionInput => {
+                if (descriptionInput) {
+                    return true;
+                } else {
+                    console.log('Please enter a description!');
+                    return false;
+                }
+            }
           },
           {
               type: 'checkbox',
@@ -51,8 +82,15 @@ const promptProject = portfolioData => {
           {
               type: 'input',
               name: 'link',
-              message: 'Would you like to feature this project',
-              default: false
+              message: 'Would you like to feature this project (Required)',
+              validate: linkInput => {
+                if (linkInput) {
+                    return true;
+                } else {
+                    console.log('Please enter a description!');
+                    return false;
+                }
+            }
           },
           {
               type: 'confirm',
